53f721992803b3bb7c45cedf9b5b611495c1c57d
vaidiksharma7278 <77123383+vaidiksharma7278@users.noreply.github.com>
2024-07-26 11:29:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://study-notion-seven-mu.vercel.app/",
 		credentials:true,
 	})
 )
